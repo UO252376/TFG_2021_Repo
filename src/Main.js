a7ca34e50ce88a97aedb2d9e4e5ab67ada591599
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-09 22:48:45 +0200

    Probando en /home/pi

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default function Main () {
   return (
 		<div>
-		<header><button>.</button></header>
+		<header><button>0</button></header>
 			<h1>¡Bienvenido!</h1>
 			<div>
 				<label>Usuario:</label>
