36342e8e4b72e1ee1fb2e2a1d9307665b6c436a5
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-11 20:01:38 +0200

    changes in server.js

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')
 // Especificar peticiones personalizadas:
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = require('socket.io').listen(server);
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 
