c3e7a7317a8a528e8b376a4e9df5431d4c321a00
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-11 18:14:41 +0200

    rollback

--- server.js
+++ server.js
@@ -12,23 +12,6 @@ app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')
 
 // Especificar peticiones personalizadas:
 
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-import BlinkLight from './src/rest/blinkingLightExample';
-io.sockets.on('connection', (socket) => {// WebSocket Connection
-    var lightvalue = 0; //static variable for current status
-    var blinkLight = new BlinkLight();
-    socket.on('light', function(data) { //get light switch status from client
-        lightvalue = data;
-        if (lightvalue) {
-            console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
-            blinkLight.switchLight();
-            socket.emit('light', lightvalue);
-        }
-    });
-});
 
 const port = 1337;
 //Run Server
