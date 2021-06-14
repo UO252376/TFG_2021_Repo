a1fd4c14a823ce125aa0d82489134386ad8599cf
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-14 19:09:11 +0200

    Rollback cambios estructura servidor

--- server.js
+++ server.js
@@ -1,24 +1,61 @@
 //Requires
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const socketIoAuth = require("socketio-auth");
 const path = require('path');
 const chalk = require('chalk');
+const morgan = require('morgan');
+//Static Routes
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use(morgan('dev')) // logging
+//Main App Route
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
 
-const express = require('express');
-const { Server } = require("socket.io");
-const socketIoAuth = require("socketio-auth");
-const onoff = require('onoff');
-const http = require('http');
 
 
-// CONSTANTES
 const port = 1337;
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-const Gpio = onoff.Gpio;
 
-const LED = new Gpio(4, 'high'); //use GPIO pin 4 as output
-const limitSwitch = new Gpio(17, 'in', 'both');
-const relay = new Gpio(18, 'low');
+//Run Server
+//app.listen(process.env.PORT || port, () => console.log(chalk.blue(`Listening intently on port ${port}`)));
+
+socketioAuth(io, { authenticate, postAuthenticate, timeout: "none" });
+
+server.listen(port, () => {
+    console.log(chalk.blue(`Socket.io listening on port ${port}`))
+});
+
+// Especificar peticiones personalizadas:
+
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+const { emit } = require('process');
+const { setInterval } = require('timers');
+var LED = new Gpio(4, 'high'); //use GPIO pin 4 as output
+var limitSwitch = new Gpio(17, 'in', 'both');
+var relay = new Gpio(18, 'low');
+
+/*
+io.on('connection', (socket) => {
+    console.log(chalk.green("User connected to socket"));
+    socket.emit('filamentStatus', limitSwitch.readSync());
+    limitSwitch.watch((err,value) => {
+        if(err) {
+            console.error(chalk.red('There was an error in the limit witch'), err);
+            return;
+        }
+        socket.emit('filamentStatus', value);
+        console.log(chalk.blue('Led change tocvalue: ${value}'));
+        LED.writeSync(value);
+    });
+    socket.on('shutdown', () => {
+        relay.writeSync(1);    
+        setTimeout(() => relay.writeSync(0), 200);
+    });
+});
+*/
 
 // CONECTAR A BASE DE DATOS
 
@@ -60,33 +97,4 @@ function postAuthenticate(socket) {
 
 }
 
-/*
-io.on('connection', (socket) => {
-    console.log(chalk.green("User connected to socket"));
-    limitSwitch.watch((err,value) => {
-        if(err) {
-            console.error(chalk.red(`There was an error in the limit witch`), err);
-            return;
-        }
-        socket.emit('filamentStatus', value);
-        console.log(chalk.blue(`Led change tocvalue: ${value}`));
-        LED.writeSync(value);
-    });
-    socket.on('shutdown', () => {
-        relay.writeSync(1);    
-        setTimeout(() => relay.writeSync(0), 200);
-    });
-});
-*/
 
-// STARTUP
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
-
-socketIoAuth(io, {authenticate, postAuthenticate});
-
-/*
-server.listen(port, () => {
-    console.log(chalk.blue(`Socket.io listening on port ${port}`));
-});
-*/
