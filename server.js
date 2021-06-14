e72b11e8e66b567df3074355fc525be487b11233
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-14 15:14:15 +0200

    Iniciando el relay a 1, invertido

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const { emit } = require('process');
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var limitSwitch = new Gpio(17, 'in', 'both');
 var relay = new Gpio(18, 'out');
-relay.writeSync(0);
+relay.writeSync(1);
 
 io.on('connection', (socket) => {
     console.log(chalk.green("User connected to socket"));
@@ -48,10 +48,10 @@ io.on('connection', (socket) => {
     });    
     socket.on('shutdown', () => {
         setTimeout(() => {
-            relay.writeSync(1);    
+            relay.writeSync(0);    
         }, 500);
         setTimeout(() => {
-            relay.writeSync(0);   
+            relay.writeSync(1);   
         }, 1000);
     });
 });
