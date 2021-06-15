1bbdad7f3ccc364a2b9a4fa9a482a69315523381
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-15 21:11:16 +0200

    Sesion storage token

--- src/Main.js
+++ src/Main.js
@@ -45,11 +45,21 @@ export default class Main extends React.Component {
 		}
 		return(
 			<div className="wrapper">
-				<header><h1>UO252376 - Controlador impresora</h1><div><button>Salir</button></div></header>
-				<TempCanvas socket={this.socket}/>
-				<FilamentStatus socket={this.socket} />
-				<ControlPanel socket={this.socket} />
-				<MessageLog socket={this.socket} />
+				{!this.state.token ? 
+					<div className="wrapper">
+						<header><h1>UO252376 - Controlador impresora</h1></header>
+						<Login setToken={this.setToken}></Login>
+					</div>
+					: 
+					<div>
+						<header><h1>UO252376 - Controlador impresora</h1><div><button>Salir</button></div></header>
+						<TempCanvas socket={this.socket}/>
+						<FilamentStatus socket={this.socket} />
+						<ControlPanel socket={this.socket} />
+						<MessageLog socket={this.socket} />
+					</div>
+				}
+				
 			</div>
 		);
 	}
