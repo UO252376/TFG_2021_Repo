cfa8bf31907ee569b37bd726fae4bb1fbc27ae25
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-15 22:00:37 +0200

    Pruebas initial state

--- src/Main.js
+++ src/Main.js
@@ -53,14 +53,14 @@ export default class Main extends React.Component {
 						<header><h1>UO252376 - Controlador impresora</h1></header>
 						<Login setToken={this.setToken}></Login>
 					</div>
-					: 
-					<div>
-						<header><h1>UO252376 - Controlador impresora</h1><div><button>Salir</button></div></header>
-						<TempCanvas socket={this.socket}/>
-						<FilamentStatus socket={this.socket} data={this.state.data} key={this.state.data}/>
-						<ControlPanel socket={this.socket} />
-						<MessageLog socket={this.socket} />
-					</div>
+					: this.state.data &&
+						<div>
+							<header><h1>UO252376 - Controlador impresora</h1><div><button>Salir</button></div></header>
+							<TempCanvas socket={this.socket}/>
+							<FilamentStatus socket={this.socket} data={this.state.data}/>
+							<ControlPanel socket={this.socket} />
+							<MessageLog socket={this.socket} />
+						</div>
 				}
 				
 			</div>
