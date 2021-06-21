d71df50aade42958ccaa423110d9748c686fbf10
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-21 17:38:12 +0200

    Seguimos con el video stream

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ io.on('connection', (socket) => {
 
 const videoApp = express();
 videoApp.listen(1338, () => console.log('listening on port ${port}!'));
-/*
+
 videoStream.acceptConnections(videoApp, {
     width: 1280,
     height: 720,
@@ -72,5 +72,5 @@ videoStream.acceptConnections(videoApp, {
     encoding: 'JPEG',
     quality: 7
 }, '/stream.mjpg', true);
-*/
+
 
