36b3457bd42b4acf159028575f95a670d5d0cf25
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-21 23:24:38 +0200

    Socket security authentication

--- src/Main.js
+++ src/Main.js
@@ -30,7 +30,7 @@ export default class Main extends React.Component {
 	}
 
 	initSockets() {
-		const socket = io({
+		const socket = io('192.168.1.21',{
 			query: {
 				token: this.state.userToken
 			}
