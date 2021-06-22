7531f736ff74a464ac3fb7c4fbc5cecbd5146f39
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-22 15:38:29 +0200

    Servidor colapsa al lanzar, probamos

--- server.js
+++ server.js
@@ -124,14 +124,13 @@ function stopStreaming() {
 // PRINTER SERIAL CONNECTION
 const SerialPort = require('serialport');
 const ReadLine = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/ttyUSB0', {autoOpen: true});
-//const lineStream = port.pipe(new ReadLine());
-/*
-port.on('open', () => {
+const serialPort = new SerialPort('/dev/ttyUSB0', {baudRate: 2500000, autoOpen: true});
+const lineStream = serialPort.pipe(new ReadLine());
+
+serialPort.on('open', () => {
     lineStream.on('data', (data) =>{ 
         io.sockets.emit('printerFeed', data);
     });
 
     port.write('M155 S1');
 });
-*/
