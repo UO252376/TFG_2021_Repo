c1acdf2be9254091a24b0c167299db91aa074b20
Juan Sánchez-Ocaña Sánchez <uo252376@uniovi.es>
2021-06-22 18:08:14 +0200

    Pasamos otra vez al servidor estandar

--- server.js
+++ server.js
@@ -76,7 +76,6 @@ io.use((socket, next) => {
     socket.on('shutdown', () => {
         relay.writeSync(1);    
         setTimeout(() => relay.writeSync(0), 200);
-        serialPort.open();
     });
 
     // STREAMING
@@ -104,14 +103,14 @@ function startStreaming(io) {
       return;
     }
    
-    var args = ["-w", "640", "-h", "480", "-o", path.join(__dirname, '/stream/image_stream.jpg'), "-t", "999999999", "-tl", "40"];
+    var args = ["-w", "640", "-h", "480", "-o", path.join(__dirname, '/stream/image_stream.jpg'), "-t", "999999999", "-tl", "100"];
     proc = spawn('raspistill', args);
    
     console.log('Watching for changes...');
    
     app.set('watchingFile', true);
    
-    fs.watchFile(path.join(__dirname, '/stream/image_stream.jpg'), {bigInt: false, persistent: true, interval: 40}, function(current, previous) {
+    fs.watchFile(path.join(__dirname, '/stream/image_stream.jpg'), {bigInt: false, persistent: true, interval: 100}, function(current, previous) {
       io.sockets.emit('liveStream', 'image_stream.jpg');
     });
 }
@@ -133,14 +132,9 @@ var REQUEST_TEMP_ID;
 
 serialPort.on('open', () => { 
     lineStream.on('data', (data) => {
-        if((data+"").indexOf("ok T") >= 0) {io.sockets.emit('printerFeed', data);}
-        if((data+"").indexOf('Error') >= 0) { serialPort.close();}
+        io.sockets.emit('printerFeed', data);
     });
     REQUEST_TEMP_ID = setInterval(() => serialPort.write('M105\n'), 8000);
-});
-
-serialPort.on('close', () => {
-    clearInterval(REQUEST_TEMP_ID);
 });
 
 
